d22c9412e99903dce2e2f4e97a81c888ed771bfe
Catherine Huang <cath.h858@gmail.com>
2018-07-13 11:50:15 -0400

    Done.

--- queries.py
+++ queries.py
@@ -1,5 +1,8 @@
 from models import *
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///music.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 def query_experimentation(session):
-    pass
-    # import pdb; pdb.set_trace()
+    pass 
